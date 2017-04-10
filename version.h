a252e76ce35ae9d57269b72bf2df9aea9bf84eea
Jamie Greunbaum <Jamie Greunbaum>
2017-04-09 23:17:38 -0400

    Corrected missing version counter script in MSVC x86 build profile.

--- version.h
+++ version.h
@@ -4,7 +4,7 @@ namespace Version
 {
     static const int MAJOR = 0;
     static const int MINOR = 6;
-    static const int REVISION = 32;
-    static const int BUILD = 2447;
+    static const int REVISION = 34;
+    static const int BUILD = 2449;
 }
 #endif // VERSION_H
