52fe3ad4540de99b7edc2b4da42a1abd503ffec8
Jamie Greunbaum <Jamie Greunbaum>
2017-04-13 00:09:26 -0400

    Expanded the size of the collision box drop-down to fit more text.

--- version.h
+++ version.h
@@ -4,7 +4,7 @@ namespace Version
 {
     static const int MAJOR = 0;
     static const int MINOR = 6;
-    static const int REVISION = 35;
-    static const int BUILD = 2463;
+    static const int REVISION = 36;
+    static const int BUILD = 2464;
 }
 #endif // VERSION_H
